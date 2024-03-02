7b0d07d813009a260a072a629a2974f506635ef9
jafiruddinkhatem <161750092+jafiruddinkhatem@users.noreply.github.com>
2024-03-02 23:06:49 +0600

    uploaded first file

---
+++ test1.js
@@ -0,0 +1,46 @@
+var a = "Ramiz"
+var b = 10
+var booldata = true 
+var c = 9
+var d = c+b 
+console.log(d)
+console.log("The data type of variable a is:", typeof(a))
+console.log("The data type of variable b is:", typeof(b))
+console.log("The data type of variable booldata is:", typeof(booldata))
+var age ="100"
+console.log(Number(age)+d)
+var e = 100
+var f = " the positon:"
+console.log(e.toString ()+f)
+//Assignment Sign (=)
+//Comparision 
+var a = 98
+var b = 98
+var c= a==b
+console.log(c)
+var fname1 = "Alex"
+var fname2 = "Jhon"
+console.log(fname1==fname2)
+var t1 = true
+var t2 = false
+console.log(t1==t2)
+// not equal
+var var1 = 89
+var var2 = 100
+
+console.log(var1<var2)
+//exam 
+
+var oddNum = 75
+var evenNum = 90
+var boolValue = oddNum !== evenNum
+console.log(boolValue)
+
+var firstName = "Jafir"
+var lastName = "Khatem"
+var fullName = firstName +" "+lastName
+console.log(fullName)
+
+var Num1 = 59
+var txt1 = "hello JS"
+console.log(Num1+txt1)
---
+++ test2_operator,logics.js
@@ -0,0 +1,92 @@
+// console.log("hellow world")
+
+
+// class 2
+
+// var prompt = require('prompt-sync')(); 
+// var num1 = prompt("enter first number: ")
+// // var num2 = prompt("enter second number: ")
+// // var sum = Number(num1)/Number(num2)
+// var reminder = num1 % 2
+// console.log("The result of divided two number is: ",reminder)
+
+// decrment operatr
+// var num2 = num1
+// num2--
+// console.log(num2)
+//conditonal statement
+
+// if (true){
+// console.log("Hi !")
+// }
+
+// var a = 20
+// if (a>18){
+//     console.log("Hi! Saad")
+// }
+
+var a = 20
+// var b = a<18
+// console.log(b)
+// if (b){
+//     console.log("Hi! Saad")
+// }
+
+// var b = a>18
+// console.log(b)
+// if (b){
+//     console.log("Hi! Saad")
+// }
+
+// var age = Number(prompt("enter your age"))
+
+// if (age < 18){
+//     console.log("you are a teenager")
+// }
+// else if (age>18){
+//     console.log("you are an adult person.")
+// }
+// // else{
+// //     console.log("you are exactly 18 ")
+// // }
+// else if (age === 18){
+//     console.log("you are exactly 18")
+// }
+// var name = prompt("enter your name: ")
+// if (name =="Ramiz"){
+//     console.log("Welcome"+ name )
+// }
+// else if (name == " Khatem "){
+//     console.log(" Welcome "+ name)
+// }
+// else{
+//     console.log(" You are not Ramiz. Please contact with him ")
+// }
+
+// console.log("I am from the bottom of the code. ")
+// var num = Number (prompt("enter a number"))
+
+// if (num%2 ==0){
+//     console.log("It's an even number")
+// }
+// else if (num%2 == 1){
+
+//     console.log("It's an odd number ")
+// }
+
+// Assignment
+
+var prompt = require('prompt-sync')(); 
+var name = prompt(" enter your name: ")
+if (name == "Jafir"){
+    console.log("Welcome!")
+}
+else if (name == "Uddin"){
+    console.log("Welcome!")
+}
+else if (name == "Khatem"){
+    console.log("Welcome!")
+}
+else {
+    console.log("Sorry! We don’t find you in the system.")
+}
